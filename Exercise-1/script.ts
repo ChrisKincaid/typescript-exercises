5390165f41eb09b107a4e7223323b2b6e2b4f9e9
ChrisKincaid <christophergkincaid@msn.com>
2023-10-01 18:15:54 -0500

    Completed: Exercise 1

---
+++ Exercise-1/script.ts
@@ -0,0 +1,6 @@
+// Declare a variable studentName of type string and assign it your name.
+// Declare a variable totalCourses of type number and assign it the number of courses you are taking.
+// Log these variables to the console.
+let studentName: string = "Chris Kincaid";
+let totalCourses: number = 4;
+console.log(studentName, totalCourses);
